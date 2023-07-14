94377fb46b9ab7d1d1b007b29598d9946854ffb0
kashafarshad <kashafarshad@hotmail.co.uk>
2023-07-14 21:10:42 +0200

    authentication request

--- src/main/java/com/spring/app/payload/request/AuthenticationRequest.java
+++ src/main/java/com/spring/app/payload/request/AuthenticationRequest.java
@@ -1,20 +1,12 @@
 package com.spring.app.payload.request;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class AuthenticationRequest {
 
 	private String username;
 	private String password;
-
-	public String getUsername() {
-		return username;
-	}
-	public void setUsername(String username) {
-		this.username = username;
-	}
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
-		this.password = password;
-	}
 }
--- src/main/java/com/spring/app/payload/request/SignupRequest.java
+++ src/main/java/com/spring/app/payload/request/SignupRequest.java
@@ -3,9 +3,13 @@ package com.spring.app.payload.request;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.Set;
- 
+
+@Getter
+@Setter
 public class SignupRequest {
     @NotBlank
     @Size(min = 3, max = 20)
@@ -15,42 +19,10 @@ public class SignupRequest {
     @Size(max = 50)
     @Email
     private String email;
-    
-    private Set<String> roles;
-    
+
     @NotBlank
     @Size(min = 6, max = 40)
     private String password;
-  
-    public String getUsername() {
-        return username;
-    }
- 
-    public void setUsername(String username) {
-        this.username = username;
-    }
- 
-    public String getEmail() {
-        return email;
-    }
- 
-    public void setEmail(String email) {
-        this.email = email;
-    }
- 
-    public String getPassword() {
-        return password;
-    }
- 
-    public void setPassword(String password) {
-        this.password = password;
-    }
-    
-    public Set<String> getRoles() {
-      return this.roles;
-    }
-    
-    public void setRole(Set<String> roles) {
-      this.roles = roles;
-    }
+
+    private Set<String> roles;
 }
